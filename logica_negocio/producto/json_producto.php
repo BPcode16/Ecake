821f4898745736d4e4a3a8e2c5ac87fda97fcd70
Brayan Palacios <pp16014@ues.edu.sv>
2021-11-16 23:19:45 -0600

    Revision por favor

--- logica_negocio/producto/json_producto.php
+++ logica_negocio/producto/json_producto.php
@@ -4,12 +4,25 @@
     $modelo=new Modelo_generico();
 
     if (isset($_GET['subir_imagen']) && $_GET['subir_imagen']=="subir_imagen_ajax") {
-        $trozos = explode(".",$_FILES['file-0']['name']);
-		$extension = end($trozos);
-		$name = "img_".$_GET['id'].".".$extension;
+        //$trozos = explode(".",$_FILES['file-0']['name']);
+		//$extension = end($trozos);
+		//$name = "img_".$_GET['id'].".".$extension;
+        $vari = '';
+		$permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ 
+			function generate_string($input, $strength = 16) {
+    			$input_length = strlen($input);
+    			$random_string = '';
+    			for($i = 0; $i < $strength; $i++) {
+        		$random_character = $input[mt_rand(0, $input_length - 1)];
+        		$random_string .= $random_character;
+    			}
+    			return $random_string;
+			}
+			$vari = generate_string($permitted_chars, 5);
 
-		$file_path = "imagenes_producto/".$name;
-        $file_path_ruta = "logica_negocio/producto/imagenes_producto/".$name;
+		$file_path = "imagenes_producto/$vari.png";
+        $file_path_ruta = "logica_negocio/producto/imagenes_producto/$vari.png";
 		try {
 			$mover = move_uploaded_file($_FILES['file-0']['tmp_name'], $file_path);
 
@@ -65,16 +78,26 @@
 
         $resultado = $modelo->actualizar_generica($array_update);
 
-        if($resultado[0]=='1' && $resultado[4]>0){
+        if($resultado[4]>0){
 
-            print json_encode(array("Exito",$_POST,$resultado));
-            exit();
+            //print json_encode(array("Exito",$_POST,$resultado));
+            //xit();
+            //aqui obtenemos el id del registro que acabamos de hacer
+            $producto_nuevo = $modelo->get_todos("tbl_productos","WHERE nombre = '".$_POST['nombre']."' ");
+
+        	print json_encode(array("Exito",$producto_nuevo[2][0],$_POST,$resultado));
+			exit();
+
+        }else if($resultado[0]=='1'){
+
+            $producto_nuevo = $modelo->get_todos("tbl_productos","WHERE nombre = '".$_POST['nombre']."' ");
+            print json_encode(array("Exito",$producto_nuevo[2][0],$_POST,$resultado));
+			exit();
+        
 
         }else{
-
             print json_encode(array("Error",$_POST,$resultado));
             exit();
-
         }
 
     }else if (isset($_POST['consultar_info']) && $_POST['consultar_info']=="si_conid_especifico") {
@@ -173,14 +196,14 @@
         //Comienza la tabla
 		$sql = "SELECT
         p.idproducto, ct.nombre idcategoria, p.nombre, p.descripcion, p.tiempoprocesamiento, p.estado, p.imagenprincipal, rl.saborrelleno idrelleno, rm.saborremojo idremojo, sb.sabortorta idsabortorta
-      FROM
-        tbl_productos p
-      LEFT JOIN tbl_categorias ct
-        ON p.idcategoria=ct.idcategoria
-      LEFT JOIN tbl_relleno rl ON p.idrelleno=rl.idrelleno
-      LEFT JOIN tbl_remojo rm ON p.idremojo=rm.idremojo
-      LEFT JOIN tbl_sabortorta sb ON p.idsabortorta=sb.idsabortorta";
-		$resultado = $modelo->get_query($sql);
+        FROM
+            tbl_productos p
+        LEFT JOIN tbl_categorias ct
+            ON p.idcategoria=ct.idcategoria
+        LEFT JOIN tbl_relleno rl ON p.idrelleno=rl.idrelleno
+        LEFT JOIN tbl_remojo rm ON p.idremojo=rm.idremojo
+        LEFT JOIN tbl_sabortorta sb ON p.idsabortorta=sb.idsabortorta";
+            $resultado = $modelo->get_query($sql);
 
 		$html=$html_tr="";
 		if ($resultado[0]=="1") {
@@ -193,7 +216,7 @@
                             <td>'.$row['nombre'].'</td>
                             <td>'.$row['descripcion'].'</td>
                             <td>'.$row['tiempoprocesamiento'].'</td>
-                            <td>'.$row['estado'].'</td>
+                            
                             <td>'.$row['idrelleno'].'</td>
                             <td>'.$row['idremojo'].'</td>
                             <td>'.$row['idsabortorta'].'</td>
@@ -213,8 +236,8 @@
                             <th>Categoría</th>
                             <th>Nombre</th>
                             <th>Descripción</th>
-                            <th>Tiempo procesado (Días)</th>
-                            <th>Estado</th>
+                            <th>Días de procesado</th>
+                            
                             <th>Relleno</th>
                             <th>Remojo</th>
                             <th>Sabor torta</th>
